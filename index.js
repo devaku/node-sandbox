c17ec3ae2725a46c9325412e26a01c841d1d4346
devaku <alejokimuy@gmail.com>
2021-11-28 10:16:35 +0800

    Added dotenv

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+// Load environment variables for debugging
+if (!process.env.NODE_ENV) {
+    require('dotenv').config();
+    console.clear();
+    console.log('Loaded Environment Variables');
+}
+
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
-});
+if (process.env.NODE_ENV.toUpperCase() === 'DEVELOPMENT') {
+    const morgan = require('morgan');
+    app.use(morgan('short'));
+}
+
+// Set routes
+require('./routes')(app);
 
 // Public folder
 app.use(express.static('public'));
